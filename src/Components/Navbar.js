4e0a2c5f971915afc3376df2da3a7b5f5fca0847
tusharkr16 <tusharkumar9871@gmail.com>
2024-01-16 13:28:45 +0530

    fixes navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,15 +5,15 @@ const Navbar = () => {
         <div>
             <nav className="navbar navbar-expand-lg bg-primary">
                 <div className="container-fluid">
-                    <a className="navbar-brand text-light" href="#">Navbar</a>
+                    <a className="navbar-brand text-light" href="/">Navbar</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
-                            <a className="nav-link text-light" href="#">Features</a>
-                            <a className="nav-link text-light" href="#">Pricing</a>
+                            <a className="nav-link active text-light" aria-current="page" href="/">Home</a>
+                            <a className="nav-link text-light" href="/">Features</a>
+                            <a className="nav-link text-light" href="/">Pricing</a>
                         </div>
                     </div>
                 </div>
